fc98ecb9abb19db7067503e1399a655faf6fc667
Alexey Starikov <alexsuperstar@mail.ru>
2018-06-28 17:53:02 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-type:application/json;charset=utf-8');
+include 'vendor/autoload.php';
+$in = new \alexstar\JsonMaker(file_get_contents('php://input'));
+if(isset($in->version)){
+    # если есть параметр version то будем считать что  входные данные в порядке
+    # формируем ответ
+    $out = new \alexstar\JsonMaker();
+    #  необходимые параметры ответа
+    $out->version = '1.0';
+    $out->response->end_session = false;
+    $out->session->session_id = $in->session->session_id;
+    $out->session->message_id = $in->session->message_id;
+    $out->session->user_id = $in->session->user_id;
+    # Ответ Алисы
+    $out->response->text='Привет';
+    # Например ссылка под текстом
+    $out->response->buttons[0]->title = 'Открыть сайт разработчика';
+    $out->response->buttons[0]->url = 'https://alexstar.ru/';
+    echo $out;
+}?>
